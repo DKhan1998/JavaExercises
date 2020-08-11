dd551fb215ff515c86f2f28e637c781c7e47cb14
Daood Khan <DKhan@qa.com>
2020-08-11 14:48:39 +0100

    successfully implemented the new excersise for looping and displaying the given number from 1 to 100 as text

--- src/main/java/com/qa/Day3/Iteration.java
+++ src/main/java/com/qa/Day3/Iteration.java
@@ -7,6 +7,8 @@ public class Iteration {
         exercise1(100);
         exercise2(100);
         exercise3();
+        exercise5();
+        numbersPrinted();
     }
 
     // loop from 100 to 199
@@ -32,11 +34,75 @@ public class Iteration {
 
     // loop through each number one to ten and print out the number as many times as the number itself
     public static void exercise3(){
-
         for(int i = 0; i < 10; i++){
            for(int e = 0; e < i; e++){
                System.out.println(i);
            }
         }
+    }
+
+    //printing numbers from int to string
+    public static void numbersPrinted() {
+        for(int i = 0; i < 100; i++) {
+            loopMechanism(i);
+        }
+    }
+
+    public static String loopMechanism(int number){
+
+        final String[] Tens = {
+                "",
+                " ten",
+                " twenty",
+                " thirty",
+                " forty",
+                " fifty",
+                " sixty",
+                " seventy",
+                " eighty",
+                " ninety"
+        };
+
+        final String[] LowerTwenty = {
+                "",
+                " one",
+                " two",
+                " three",
+                " four",
+                " five",
+                " six",
+                " seven",
+                " eight",
+                " nine",
+                " ten",
+                " eleven",
+                " twelve",
+                " thirteen",
+                " fourteen",
+                " fifteen",
+                " sixteen",
+                " seventeen",
+                " eighteen",
+                " nineteen"
+        };
+
+        String Overall;
+
+        if (number % 100 < 20) {
+            Overall = LowerTwenty[number % 100];
+            number /= 100;
+        } else {
+            Overall = LowerTwenty[number % 10];
+            number /= 10;
+
+            Overall = Tens[number % 10] + Overall;
+            number /= 10;
+        }
+        if (number == 0) System.out.println(Overall);
+        return Overall;
+    }
+
+    public static void exercise5(){
+
     }
 }
