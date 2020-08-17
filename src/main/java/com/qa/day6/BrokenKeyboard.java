990552c60e0458bf3e2fd41fe05dae41a27778f1
Daood Khan <DKhan@qa.com>
2020-08-17 09:44:45 +0100

    code somehown workds for first loop need to text wether all broken keys have been tested.

--- src/main/java/com/qa/day6/BrokenKeyboard.java
+++ src/main/java/com/qa/day6/BrokenKeyboard.java
@@ -2,17 +2,15 @@ package com.qa.day6;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 
 public class BrokenKeyboard {
 
-    public static List<String> foundWords = new ArrayList<String>();
+    public static List<String> foundWords = new ArrayList<>();
 
     public static void main(String[] args) {
-        String[] workingKeys = {"abcd", "qwer", "hjklo"};
+        String[] workingKeys = {"abcd", "zyav", "hjklo"};
         // FInd the file and print out the lines in the file
         try (BufferedReader br = new BufferedReader(new FileReader("src/main/java/com/qa/day6/enable1.txt"))) {
             System.out.println("File can be read ! ");
@@ -24,54 +22,57 @@ public class BrokenKeyboard {
 
     public static void loopCombination(String[] input, BufferedReader file) throws IOException {
 
-        int maxWords;
-        int foundChars;
-        boolean[] charsFound;
-        boolean allCharFound;
-
-        // loop until all characters from input array have been found before moving on
+        // controls the number of words to accept
+        int maxWords = 0;
+        // tracks the amount of chars found within the txt file line
+        int foundChars = 0;
         String line;
-        // loop through each line in the text file
-        while ((line = file.readLine()) != null) {
-            System.out.println("Current Line Loop : " + line);
-            // Loop through individual string in the input array
-            for (String brokenKeys : input) {
-                maxWords = 0;
-                System.out.println("Loop one (input string): " + brokenKeys);
-                // repeat process until found all words containing all characters
-                if(maxWords < brokenKeys.length()){
-                    foundChars = 0;
-                    charsFound = new boolean[brokenKeys.length()];
-                    System.out.println("Loop two (condition) = " + Arrays.toString(charsFound));
+
+        // loop through each line in the input
+        for (String brokenKeys : input) {
+            System.out.println("---------------------------------------------------");
+            System.out.println("Loop one (input string): " + brokenKeys);
+            // check if we have a max of 5 words per input
+            if(maxWords <= 5) {
+                System.out.println("---------------------------------------------------");
+                System.out.println("Does input string contain 5 words? " + maxWords);
+                // Loop through individual lines in the input array
+                while ((line = file.readLine()) != null){
+                    System.out.println("---------------------------------------------------");
+                    System.out.println("Text Line Loop, Reading line " + line);
                     // loop through each character in the input array string
                     for (char inputChar : brokenKeys.toCharArray()) {
-                        System.out.println("Loop three (looped character) : " + inputChar);
+                        System.out.println("---------------------------------------------------");
+                        System.out.println("Loop three (looped character from user input) : " + inputChar);
                         // check to see if character already has been checked
                         // loop though each character on the given line
                         for (char lineChar : line.toCharArray()) {
+                            System.out.println("---------------------------------------------------");
+                            System.out.println("Loop four (looped character from text file line)  = " + lineChar);
                             // check if looped characters are equal
-                            if (inputChar == lineChar) {
-                                charsFound[foundChars] = true;
-
-                                System.out.println(inputChar + " becomes : " + line);
-                                System.out.println("number of chars found : " + charsFound.length);
-                                System.out.println("character found : " + Arrays.toString(charsFound));
-                            }
-                            else charsFound[foundChars] = false;
-
-                        }
-                        for (boolean check : charsFound) {
-                            if (check) {
+                            if (inputChar == lineChar && foundChars < brokenKeys.length()) {
+                                System.out.println("---------------------------------------------------");
+                                System.out.println("New character found to match  = " + inputChar);
                                 foundChars++;
+                                System.out.println("Added new character to foundChar  = " + foundChars);
+                                maxWords++;
+                                System.out.println("Added new wordCount to maxWords, new value  = " + maxWords);
+                                foundWords.add(line);
+                                System.out.println("New word added : " + foundWords);
+                                break;
+                            } else {
+                                System.out.println("---------------------------------------------------");
+                                System.out.println("NO MATCH !!");
                             }
                         }
-                        maxWords++;
                     }
-                } else {
-                    foundWords.add(line);
-                    System.out.println("New word added : " + foundWords);
-
                 }
+                foundChars = 0;
+                maxWords = 0;
+            } else {
+                System.out.println("---------------------------------------------------");
+                System.out.println("Number of matches found is larger than 5!");
+                foundChars = 0;
             }
         }
 
